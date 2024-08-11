a5fb2d2427f0350583b4b747c2cab2c8716150ae
Anmol Gangwar <anmolgangwar64@gmail.com>
2024-08-12 01:24:31 +0530

    chore :sparkles: added prisma.schema and data modelling

--- src/app/worker/processors/inbound-mail.processor.ts
+++ src/app/worker/processors/inbound-mail.processor.ts
@@ -8,6 +8,8 @@ import {
   VisitorViewMailJob,
 } from 'src/libraries/queue/jobs/job.payload';
 import { INBOUND_MAIL_PARSE_QUEUE } from 'src/libraries/queue/queue.constants';
+import { simpleParser } from 'mailparser';
+
 @Processor(INBOUND_MAIL_PARSE_QUEUE, {
   concurrency: 50,
   useWorkerThreads: true,
@@ -29,7 +31,11 @@ export class InboundMailProcessor extends WorkerHost {
       switch (job.name) {
         // for visitor views
         case QueueEventJobPattern.MAIL_VISITOR_VIEW:
-          console.log(job.data);
+          const rawMail = JSON.parse(job.data.rawMail);
+          const parsedEmail = await simpleParser(rawMail);
+          console.log('slug:', job.data.slug);
+          console.log('Parsed Email:', parsedEmail);
+          // console.log(job.data);
           break;
         case QueueEventJobPattern.MAIL_MEMBER_VIEW:
           console.log(job.data);
@@ -44,6 +50,7 @@ export class InboundMailProcessor extends WorkerHost {
       throw error; // Throwing the error will cause the job to be re-queued for retry
     }
   }
+
   @OnWorkerEvent('completed')
   async onCompleted(
     job: Job<VisitorViewMailJob['data'] | MemberViewMailJob['data']>,
